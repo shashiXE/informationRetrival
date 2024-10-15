d4e188b12574bd996e84107cbed7e24c4f90e5e8
Shashi Mishra <shamishr@WKMZT5020479.global.publicisgroupe.net>
2024-10-15 22:48:47 +0100

    added more code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI',
+    version= '0.0.0',
+    author= 'Shashi K',
+    author_email= 'shashilion2002@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
